39c87cea386fe944ae3432bd9c4744414b26a42b
giyun <patronymic@naver.com>
2019-08-23 20:21:33 +0900

    Create gradle project

---
+++ src/main/java/com/giyun/errorsearch/ErrorsearchApplication.java
@@ -0,0 +1,13 @@
+package com.giyun.errorsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErrorsearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ErrorsearchApplication.class, args);
+    }
+
+}
